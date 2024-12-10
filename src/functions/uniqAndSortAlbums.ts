d6f816f9eab10d329757652bb23248017b7cb666
Laurent Binder <laurentbinder@gmail.com>
2024-12-10 17:20:00 +0100

    more filters

--- src/functions/uniqAndSortAlbums.ts
+++ src/functions/uniqAndSortAlbums.ts
@@ -3,18 +3,24 @@ import { ReleasesTypes } from '../models/discogsTypes';
 const uniqAndSortAlbums = (albums: ReleasesTypes[]) => {
   // remove album with undefined year
   const albumsWithReleasedYear = albums.filter(
-    (album) => album.year !== undefined
+    (album) =>
+      album.barcode.length !== 0 &&
+      album.year !== undefined &&
+      album.thumb !== ''
   );
 
   // TODO add albums that are not on Discogs
-  // ...
+  // ...push me in :)
 
   const uniqueResults = [
     ...new Map(
       albumsWithReleasedYear.map((release) => [release.master_id, release])
     ).values(),
   ];
-  console.log('🚀 ~ uniqAndSortAlbums ~ uniqueResults:', uniqueResults.length);
+  console.log(
+    '🚀 ~ uniqAndSortAlbums ~ uniqueResults:',
+    albumsWithReleasedYear
+  );
   // sort by year
   const sortedResults = uniqueResults.sort((a, b) => b.year - a.year);
 
