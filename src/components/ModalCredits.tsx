4b924f8c57ebf1cc3050d0714a23caf50b6c0450
Laurent Binder <laurentbinder@gmail.com>
2024-12-13 20:45:06 +0100

    modal css

--- src/components/ModalCredits.tsx
+++ src/components/ModalCredits.tsx
@@ -18,21 +18,21 @@ export const ModalCredits: FC<ModalCreditsProps> = ({
     <Modal
       isOpen={!!artistRole && !!albumDetails}
       onRequestClose={handleCloseModal}
-      overlayClassName='fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center'
-      className='text-center rounded-sm focus:outline-none'
+      overlayClassName='fixed flex justify-center items-center inset-0 bg-black bg-opacity-50'
+      className='justify-items-center text-center rounded-sm focus:outline-none'
     >
-      <div className='w-[600px] md:w-96 sm:w-80 bg-white flex flex-col items-center py-6 px-2 gap-6'>
+      <div className='w-3/4 lg:w-[600px]   bg-white flex flex-col items-center py-6 px-2 gap-6'>
         <img
           src={albumDetails.images[0].resource_url}
           alt={albumDetails.title}
-          className='w-[500px] md:w-80 sm:w-72 rounded-sm shadow-[8px_12px_20px_0px_rgba(0,0,0,0.5)] object-cover'
+          className='w-3/4 lg:w-[500px] rounded-sm shadow-[8px_12px_20px_0px_rgba(0,0,0,0.5)] object-cover'
         />
         <div className='flex flex-col justify-center gap-4'>
           <div>
-            <h2 className='font-dreadnoughtus tracking-widest text-3xl uppercase font-bold mb-1'>
+            <h2 className='font-dreadnoughtus tracking-widest text-2xl sm:text-3xl uppercase font-bold mb-1'>
               {albumDetails.title}
             </h2>
-            <h2 className='text-xl uppercase font-semibold '>
+            <h2 className='text-lg sm:text-xl uppercase font-semibold '>
               {removeParenthesesAndNumbers(albumDetails.artists_sort)}
             </h2>
           </div>
